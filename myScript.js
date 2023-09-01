813bfbd1504680faffbb4df1b451d4c9f9c83a8b
Christian Zawojski <clzawojski@gmail.com>
2023-08-31 20:05:26 -0400

    completed up to start of step 3 in project

---
+++ myScript.js
@@ -0,0 +1,6 @@
+// randomly returns ‘Rock’, ‘Paper’ or ‘Scissors’ to make the computer’s play
+function getComputerChoice() {
+    // return "rock" "paper" or "scissors";
+}
+
+console.log(getComputerChoice());
