a7de8c47ac1d0fcba28366d258571cc213d42078
Christian Zawojski <clzawojski@gmail.com>
2023-09-04 12:11:42 -0400

    moved prompt and computer choice inside playround function -- and looped over game function five times

--- myScript.js
+++ myScript.js
@@ -9,44 +9,49 @@ function getComputerChoice() {
     return textArray[randomNumber];
 }
 
-const computerSelection = getComputerChoice();
-
-console.log(computerSelection);
-
-let playerSelection = prompt("Choose one: rock, paper, scissors.");
 // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-playerSelection = playerSelection.toLowerCase();
+
+// let playerselection ... through ... playerSelection.toLowerCase works HERE
 
 // plays a single round of Rock Paper Scissors -- two parameters - the playerSelection and computerSelection
+
 // return a string that declares the winner of the round like so: "You Lose! Paper beats Rock" 
-// Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-// function game () {
-    // for (let i = 0; i < 5; i++) {
-    function playRound(playerSelection, computerSelection) {
-        let result;
-        if (playerSelection != null) {
-            if (playerSelection === 'rock' && computerSelection === 'paper') {
-            result = 'You lose! Paper covers rock.';
-            }
-            else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-                result = 'You win! Rock crushes scissors.';
-            }
-            else if (playerSelection === 'paper' && computerSelection === 'rock') {
-                result = 'You win! Paper covers rock.';
-            }
-            else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-                result = 'You lose! Scissors cut paper.';
-            }
-            else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-                result = 'You lose! Rock crushes scissors.';
-            }
-            else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-                result = 'You win! Scissors cut paper.';
-            }
-            else if (playerSelection === computerSelection) {
-                result = 'Tie! No winner here.';
-            }
-            return result;
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Choose one: rock, paper, scissors.");
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    let result;
+    if (playerSelection != null) {
+        if (playerSelection === 'rock' && computerSelection === 'paper') {
+        result = 'You lose! Paper covers rock.';
         }
+        else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            result = 'You win! Rock crushes scissors.';
+        }
+        else if (playerSelection === 'paper' && computerSelection === 'rock') {
+            result = 'You win! Paper covers rock.';
+        }
+        else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+            result = 'You lose! Scissors cut paper.';
+        }
+        else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+            result = 'You lose! Rock crushes scissors.';
+        }
+        else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+            result = 'You win! Scissors cut paper.';
+        }
+        else if (playerSelection === computerSelection) {
+            result = 'Tie! No winner here.';
+        }
+        return result;
     }
-console.log(playRound(playerSelection, computerSelection));
+}
+
+// Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+    game();
