4881e66ee7cfe9ab9699788fc454abc7e6224b4d
Christian Zawojski <clzawojski@gmail.com>
2023-09-05 20:36:24 -0400

    plays 5 times and keeps score

--- myScript.js
+++ myScript.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 // randomly returns ‘Rock’, ‘Paper’ or ‘Scissors’ to make the computer’s play
 function getComputerChoice() {
     const textArray = [
@@ -25,40 +28,51 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = getComputerChoice();
     console.log(computerSelection);
-    let result;
+    // let result;
     if (playerSelection != null) {
         if (playerSelection === 'rock' && computerSelection === 'paper') {
-            result = 'You lose! Paper covers rock.';
+            computerScore++;
+            return 'You lose! Paper covers rock.'
         }
         else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            result = 'You win! Rock crushes scissors.';
+            playerScore++;
+            return 'You win! Rock crushes scissors.'
         }
         else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            result = 'You win! Paper covers rock.';
+            playerScore++;
+            return 'You win! Paper covers rock.'
         }
         else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            result = 'You lose! Scissors cut paper.';
+            computerScore++;
+            return 'You lose! Scissors cut paper.'
         }
         else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            result = 'You lose! Rock crushes scissors.';
+            computerScore++;
+            return 'You lose! Rock crushes scissors.'
         }
         else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            result = 'You win! Scissors cut paper.';
+            playerScore++;
+            return 'You win! Scissors cut paper.'
         }
         else if (playerSelection === computerSelection) {
-            result = 'Tie! No winner here.';
+            return 'Tie! No winner here.'
         }
-        return result;
+        // return result;
     }
 }
 
 // Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 function game() {
     for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection));
+        let x = playRound(playerSelection, computerSelection);
+        console.log(x);
     }
 }
+
     game();
+
+    console.log('Your score: ' + playerScore);
+    console.log('Computer score: ' + computerScore);
 
     // NEXT STEP: learn how to keep score
     
